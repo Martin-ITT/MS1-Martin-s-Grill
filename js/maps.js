e08dbd21d0a06e983826ca57bfc25bc5d2411c08
Martin <x00135214@gmail.com>
2020-12-09 23:08:06 +0000

    js from contact.html isertes into separate files

---
+++ js/maps.js
@@ -0,0 +1,11 @@
+/* Gogle maps */
+
+function initMap(){
+    var options = {
+        zoom:14,
+        center:{lat:53.3402575,lng:-6.2637580}
+    }
+    var map = new
+    google.maps.Map(document.getElementById('map'), options);
+}
+    
---
+++ js/my-slick.js
@@ -0,0 +1,10 @@
+
+$(document).ready(() => {
+    $('#contact-slideshow .slick').slick({
+        autoplay:true,
+        fade: true,
+        autoplaySpeed: 3000,
+        speed: 2000,
+
+    }); 
+});
